b8bd82ff8b6fcf22ad90fbf346201c8f329f9380
Mateusz Matczak <35334792+matm616@users.noreply.github.com>
2026-09-07 14:57:03 +0100

    feat: add useRfFlightsCounter option to settings
    Allows use to choose whether to use flights widget or counter in RF for number of flights

--- SCRIPTS/RF2-dashboards/rf2_dashboard_opt.lua
+++ SCRIPTS/RF2-dashboards/rf2_dashboard_opt.lua
@@ -1,27 +1,29 @@
 local M = {
 
     options = {
-        {"guiStyle"     , CHOICE, 1 , {
+        {"guiStyle"           , CHOICE, 1 , {
             "1-Fancy",
             "2-Modern",
             "3-Nitro",
         }},
-        {"guiStylePost" , CHOICE, 3 , {
+        {"guiStylePost"       , CHOICE, 3 , {
             "Same as Flight Dashboard",
             "Summary 1",
             "Summary 2",
         }},
-        {"enableAudio"     , BOOL  ,   1         }, -- 0=disable audio announcements, 1=enable audio announcements
-        {"showTotalVoltage", BOOL  ,   0         }, -- 0=Show as average Lipo cell level, 1=show the total voltage (voltage as is)
-        {"reserve_capa"    , VALUE ,   0, 0, 40  },
-        {"currTop"         , VALUE , 200, 60,400 },
-        {"tempTop"         , VALUE ,  80, 30,150 },
-        {"textColor"       , COLOR , WHITE       },
+        {"enableAudio"        , BOOL  ,   1         }, -- 0=disable audio announcements, 1=enable audio announcements
+        {"showTotalVoltage"   , BOOL  ,   0         }, -- 0=Show as average Lipo cell level, 1=show the total voltage (voltage as is)
+        {"useRfFlightsCounter", BOOL  ,   0         }, -- 0=use flights widget counter, 1=use RF2 flights counter
+        {"reserve_capa"       , VALUE ,   0, 0, 40  },
+        {"currTop"            , VALUE , 200, 60,400 },
+        {"tempTop"            , VALUE ,  80, 30,150 },
+        {"textColor"          , COLOR , WHITE       },
     },
 
     translate = function(name)
         local translations = {
             showTotalVoltage="Show Voltage as Total",
+            useRfFlightsCounter="Use RF2 Flights Counter (If available)",
             -- enableCapa="Enable Capacity",
             -- useTelemetry="Use Telemetry (faster update)",
             guiStyle="On Flight Dashboard",
